b12a65916f048b5a2ee299a2304eeef838085d5a
Arnesh <arneshie@gmail.com>
2021-06-14 15:33:42 -0400

    Updates

---
+++ precache-manifest.7190ed32dbc57642930549154fcd2bab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f78dca45954978843e90df3effeaed1",
+    "url": "/unilab-website/index.html"
+  },
+  {
+    "revision": "df1684994b5432688dff",
+    "url": "/unilab-website/static/css/main.474ddbc6.chunk.css"
+  },
+  {
+    "revision": "36128ff7f373e5beff7a",
+    "url": "/unilab-website/static/js/2.1442e098.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/unilab-website/static/js/2.1442e098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df1684994b5432688dff",
+    "url": "/unilab-website/static/js/main.8c5906e9.chunk.js"
+  },
+  {
+    "revision": "7e2a18bb71784d53e608",
+    "url": "/unilab-website/static/js/runtime-main.8d15b3dd.js"
+  }
+]);
